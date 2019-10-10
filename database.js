615d47edeea2c7fb0b3daecdbef64b2f217d3c4d
Demonic_30 <koffareza007@gmail.com>
2019-10-10 22:35:50 +0700

    insert complete

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const pgp = require('pg-promise')();
 var db = pgp('postgres://dpypebjgwshfqv:54aecf7e68a2751929d3238e772ab2a99b4e583d88c817bdeebb0fd09d8b5f89@ec2-54-235-104-136.compute-1.amazonaws.com:5432/d4p6ukgjp7bm8q?ssl=true');
 
 function getAllQuestions(req, res) {
-    db.any('select * from questions')
+    db.any('select * from questions where number = 3')
         .then(function (data) {
             res.status(200)
                 .json({
@@ -59,19 +59,8 @@ function getAllQuestions(req, res) {
 function insertQuestions(req, res) {
     console.log(req.body)
     db.any(`insert into questions(image1, image2, image3, image4, number, question, question_type, answer1, answer2, answer3, answer4, no_question, status)` +
-        `values(${image1}, ${image2}, ${image3}, ${image4}, ${number}, ${question}, ${question_type}, ${answer1}, ${answer2}, ${answer3}, ${answer4}, ${no_question}, ${status})`, { image1: 'http://www.clipartbest.com/cliparts/9iR/LX4/9iRLX4k4T.jpg',
-        image2: 'https://www.logolynx.com/images/logolynx/00/00223e5ceabf9dcaf376f11fc67d2607.jpeg',
-        image3: 'null',
-        image4: 'null',
-        number: '3',
-        question: 'เพศของคุณ',
-        question_type: 'quick_reply',
-        answer1: 'ชาย',
-        answer2: 'หญิง',
-        answer3: 'null',
-        answer4: 'null',
-        no_question: '1',
-        status: '0' })
+        `values('${req.body.image1}', '${req.body.image2}', '${req.body.image3}', '${req.body.image4}', '${req.body.number}', '${req.body.question}', '${req.body.question_type}', '${req.body.answer1}', '${req.body.answer2}', '${req.body.answer3}', '${req.body.answer4}', '${req.body.no_question}', '${req.body.status}')` 
+         )
         .then(function (data) {
             res.status(200)
                 .json({
@@ -85,11 +74,14 @@ function insertQuestions(req, res) {
             res.status(500)
                 .json({
                     status: 'Failed',
-                    message: 'Failed to insert products id:' + req.params.id
+                    message: 'Failed to insert Questions :' + req.params.number
                 })
         })
 
 }
+
+// db.any("insert into questions (image1, image2, image3, image4, number, question, question_type, answer1, answer2, answer3, answer4, no_question, status)"+
+//     "values ('"+req.body.image1+"','"+req.body.image2+"','"+req.body.number+"','"+req.body.question+"','"+req.body.question_type+"','"+req.body.answer1+"','"+req.body.answer2+"','"+req.body.no_question+"','"+req.body.status+"')")
 
 // function getProductByID(req, res) {
 //     db.any('select * from products where id =' + req.params.id)
