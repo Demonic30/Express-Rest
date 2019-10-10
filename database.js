0a86cedc2876ebdea926f644769fe5462ee4e3c1
Demonic_30 <koffareza007@gmail.com>
2019-10-10 22:44:07 +0700

    edit insert

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const pgp = require('pg-promise')();
 var db = pgp('postgres://dpypebjgwshfqv:54aecf7e68a2751929d3238e772ab2a99b4e583d88c817bdeebb0fd09d8b5f89@ec2-54-235-104-136.compute-1.amazonaws.com:5432/d4p6ukgjp7bm8q?ssl=true');
 
 function getAllQuestions(req, res) {
-    db.any('select * from questions where number = 3')
+    db.any('select * from questions')
         .then(function (data) {
             res.status(200)
                 .json({
